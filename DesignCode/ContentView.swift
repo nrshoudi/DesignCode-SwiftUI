c50813ac09f5cc1060bb715c17dc07881a45d9ca
Noura <n.rshoudi@gmail.com>
2020-05-10 01:49:43 +0300

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Noura Abdualaziz on 10/05/2020.
+//  Copyright © 2020 Noura Abdualaziz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
